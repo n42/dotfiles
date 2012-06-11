401cff0543fa3e223d70cb765b9e8b7fbf140d83
Andreas Nordin <anordin@n42.se>
2012-06-11 22:49:09 +0200

    initial dotfile commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Dotfiles
+========
+My dotfile repo. Use at your own peril.
+
+License
+-------
+As part of a recent dotfile overhaul along with discovering tmux, I nicked the deployment concept, much of the bashrc, and much of the tmux conf from [tejr][1]. As such, they're distributed under an [MIT License][2], Copyright (c) [Andreas Nordin][3] and [Tom Ryder][4].
+
+[1]:https://github.com/tejr
+[2]:http://www.opensource.org/licenses/MIT
+[3]:http://github.com/n42
+[4]:http://www.sanctum.geek.nz/about/tom-ryder
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check out submodules
+# cd "${HOME}/.dotfiles"
+# git submodule --quiet update --init
+# cd "${OLDHOME}"
+
+# Link in files, replacing whatever's there
+ln -fs ".dotfiles/bash/bashrc" "${HOME}/.bashrc"
+ln -fs ".dotfiles/bash/bash_functions" "${HOME}/.bash_functions"
+ln -fs ".dotfiles/bash/bash_aliases" "${HOME}/.bash_aliases"
+
+ln -fs ".dotfiles/tmux/tmux.conf" "${HOME}/.tmux.conf"
+
+ln -fs ".dotfiles/git/gitconfig" "${HOME}/.gitconfig"
+
+ln -fs ".dotfiles/vim/vimrc" "${HOME}/.vimrc"
